040a4d9e936485e2f1720b7550e0b7296fbc731f
Jesus Rodriguez <jesusrod789@gmail.com>
2024-02-10 14:41:33 -0800

    finish chp 15: Food Order App

---
+++ 15. Food Order App/01-starting-project/src/hooks/useHttp.js
@@ -0,0 +1,57 @@
+import { useEffect, useState, useCallback } from "react";
+
+// in general dealing with request
+async function sendHttpRequest(url, config) {
+  const response = await fetch(url, config);
+
+  const resData = await response.json();
+
+  if (!response.ok) {
+    throw new Error(
+      resData.message || "Something went wrong, failed to send data"
+    );
+  }
+
+  return resData;
+}
+
+export default function useHttp(url, config, initialData) {
+  const [data, setData] = useState(initialData);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+
+  function clearData() {
+    setData(initialData);
+  }
+
+  // updates state based on request status
+  const sendRequest = useCallback(
+    async function sendRequest(data) {
+      setIsLoading(true);
+      try {
+        const resData = await sendHttpRequest(url, { ...config, body: data });
+        setData(resData);
+      } catch (error) {
+        setError(error.message || "Something went wrong!");
+      }
+
+      setIsLoading(false);
+    },
+    [url, config]
+  );
+
+  // when using useHttp this will call sendRequest() with basic settings to just GET data from backend
+  useEffect(() => {
+    if ((config && (config.method === "GET" || !config.method)) || !config) {
+      sendRequest();
+    }
+  }, [sendRequest, config]);
+
+  return {
+    data,
+    isLoading,
+    error,
+    sendRequest,
+    clearData,
+  };
+}
