ec5a65e6b746b0bee86dfc68b515b7592e941590
Jesus Rodriguez <jesusrod789@gmail.com>
2024-03-14 18:58:29 -0700

    finish chp 17: Advance Redux

---
+++ 17. Advance Redux/01-starting-project/src/components/Cart/CartButton.js
@@ -0,0 +1,24 @@
+import { useDispatch, useSelector } from "react-redux";
+
+import { uiActions } from "../../store/ui-slice";
+import classes from "./CartButton.module.css";
+
+const CartButton = (props) => {
+  const dispatch = useDispatch();
+  const cartQuantity = useSelector(state => state.cart.totalQuantity)
+
+  const toggleCartHandler = () => {
+    // so uiActions.functionName makes an action object becayse the functions are action creator objects 
+    // then the dispatch takes in an action object and carries out the function 
+    dispatch(uiActions.toggle());
+  };
+
+  return (
+    <button className={classes.button} onClick={toggleCartHandler}>
+      <span>My Cart</span>
+      <span className={classes.badge}>{cartQuantity}</span>
+    </button>
+  );
+};
+
+export default CartButton;
